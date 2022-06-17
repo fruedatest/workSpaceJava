72a965e2dfa64945c8f853def490315773a98fd0
FruegaGit <fruedagit@gmail.com>
2022-06-17 16:35:50 +0200

    messageDialog

---
+++ E47/src/mainPackage/Main.java
@@ -0,0 +1,17 @@
+package mainPackage;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		//JOptionPane.showConfirmDialog(null, "Cuerpo del mensaje");
+		//JOptionPane.showConfirmDialog(null, "Aceptar o cancelar", "OK_CANCEL_OPTION", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		//JOptionPane.showConfirmDialog(null, "Aceptar", "CLOSE_OPTION", JOptionPane.CLOSED_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		//JOptionPane.showConfirmDialog(null, "Sí o no", "YES_NO_OPTION", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		//JOptionPane.showConfirmDialog(null, "Sí, no o Cancelar", "YES_NO_CANCEL_OPTION", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE);
+		//JOptionPane.showConfirmDialog(null, "Mensaje de información", "INFORMATON_MESSAGE", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showConfirmDialog(null, "Mensaje de error", "ERROR_MESSAGE", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+	
+	}
+}
